79d6b53d4f8a4af7a3532ee73f232e168949c31c
charlenetan <tanwsc@gmail.com>
2021-07-02 10:13:52 +0800

    second commit test

--- src/Components/test.js
+++ src/Components/test.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { useState, useEffect } from "react";
 
-const apikey = "d9d10c6f-db49-492f-b90f-6319d07b951f";
+const apikey = process.env.REACT_APP_APIKEY;
 const harvardArtMuseumApi = `https://api.harvardartmuseums.org/object?apikey=${apikey}&size=5`;
 
 const APITest = () => {
