f8f65b82219fa95facdbcd3f47219d250e0873f9
charlenetan <tanwsc@gmail.com>
2021-07-02 12:18:01 +0800

    render test

--- src/Components/test.js
+++ src/Components/test.js
@@ -2,10 +2,18 @@ import * as React from "react";
 import { useState, useEffect } from "react";
 
 const apikey = process.env.REACT_APP_APIKEY;
-const harvardArtMuseumApi = `https://api.harvardartmuseums.org/object?apikey=${apikey}&size=5`;
+const harvardArtMuseumApi = `https://api.harvardartmuseums.org/object?apikey=${apikey}&classification=75&size=1`;
 
 const APITest = () => {
-  const [data, setData] = useState();
+  const [data, setData] = useState({
+    title: "",
+    period: "",
+    date: "",
+    classification: "",
+    medium: "",
+    dimensions: "",
+    image: "",
+  });
 
   useEffect(() => {
     fetch(harvardArtMuseumApi)
@@ -18,11 +26,29 @@ const APITest = () => {
       })
       .then((data) => {
         console.log(data);
-        setData(data);
+        setData({
+          title: data.records[0].title,
+          period: data.records[0].period,
+          date: data.records[0].dated,
+          classification: data.records[0].classification,
+          medium: data.records[0].medium,
+          dimensions: data.records[0].dimensions,
+          image: data.records[0].primaryimageurl,
+        });
       });
   }, []);
 
-  return <div>{JSON.stringify(data)}</div>;
+  return (
+    <div>
+      <h3>{data.title}</h3>
+      <p>{data.period}</p>
+      <p>{data.date}</p>
+      <p>{data.classification}</p>
+      <p>{data.medium}</p>
+      <p>{data.dimensions}</p>
+      <img src={data.image} alt="img" />
+    </div>
+  );
 };
 
 export default APITest;
