fa40a6a4d4ec5a7681fb0fe07ef699cc5dbc3c81
charlenetan <tanwsc@gmail.com>
2021-07-03 17:21:14 +0800

    mcleaning up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
 
-import APITest from "./Components/test.js";
+import ArtObject from "./Components/ArtObject.js";
 
 function App() {
   return (
     <div className="App">
       <h1>render</h1>
-      <APITest />
+      <ArtObject />
     </div>
   );
 }
