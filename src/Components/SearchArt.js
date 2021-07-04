a1acd19d00790501f9102b3823fbd57f9d18d8e2
charlenetan <tanwsc@gmail.com>
2021-07-04 21:51:48 +0800

    minor edits to proxy

--- src/Components/SearchArt.js
+++ src/Components/SearchArt.js
@@ -1,7 +1,10 @@
 import * as React from "react";
+import { useRef } from "react";
 import { Link } from "react-router-dom";
 
 const SearchArt = () => {
+  const inputSearchRef = useRef();
+
   const linkStyle = {
     margin: "10px 10px",
     padding: "10px",
@@ -12,10 +15,32 @@ const SearchArt = () => {
     textDecoration: "none",
   };
 
+  // get input word
+  const handleSearch = () => {
+    console.log(inputSearchRef.current.value);
+
+    // const makeAPICall = async () => {
+    //   setStatus("loading");
+    //   try {
+    //     const res = await fetch(`${harvardArtMuseumUrl}title=${searchKey}`);
+    //     const searchResult = await res.json();
+    //     setStatus("resolved");
+    //     console.log(searchResult.records);
+    //   } catch (error) {
+    //     console.log(error);
+    //   }
+    // };
+  };
+
   return (
     <div className="search">
       <p>Search for some art</p>
-      <input type="text" placeholder="type a keyword"></input>
+      <input
+        type="text"
+        ref={inputSearchRef}
+        placeholder="title keyword"
+      ></input>
+      <button onClick={handleSearch}>Search</button>
       <br />
       <Link to="/" style={linkStyle}>
         Back to Home
